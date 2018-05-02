c0b06e969ad07b7e7383e93e74a30b922e4c51ee
Ryan Viajedor <ryan.viajedor1990@gmail.com>
2018-05-02 18:24:06 +0800

    May 02, 2018

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from core.models import *
 
 # Create your views here.
 # function based view
@@ -10,8 +11,48 @@ def home_view(request):
 
 
 def dine_view(request):
+    categories = ProductCategory.objects.all()
+    # beverage = ProductCategory.objects.get(description='Beverages')
+    # appetizer = ProductCategory.objects.get(description='Appetizer')
+    # salad = ProductCategory.objects.get(description='Salad')
+    # main = ProductCategory.objects.get(description='MainDish')
+    products = Product.objects.all()
+
+    print(categories)
+    for category in categories:
+        if category.description == 'Beverages':
+            beverages = Product.objects.filter(productCategoryID=1)
+            # print(beverages)
+
+        if category.description == 'Appetizer':
+            appetizers = Product.objects.filter(productCategoryID=2)
+
+        if category.description == 'Salad':
+            salads = Product.objects.filter(productCategoryID=3)
+
+        if category.description == 'MainDish':
+            maindishes = Product.objects.filter(productCategoryID=4)
+
+    # prod = []
+    # for category in categories:
+    #     prodcts = Product.obects.get(productCategoryID=category.id)
+    #     print(prodcts)
+    #     product = {
+    #         'category_description': category.description,
+    #         'product_description': products.description
+    #     }
+
+    #     prod.append(product)
+
+
+    print(beverages)
+    print(appetizers)
+    print(salads)
+    print(maindishes)
+
+
     template='dine.html'
-    context = {}
+    context = {'categories': categories, 'beverages': beverages, 'appetizers': appetizers, 'salads': salads, 'maindishes': maindishes}
     return render(request, template, context)
 
 
